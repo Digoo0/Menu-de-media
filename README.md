9def09c7a60e752d42d5e72c527267cac1edf143
Digoo0 <155112237+Digoo0@users.noreply.github.com>
2025-05-06 01:52:06 +0000

    Code

---
+++ README.md
@@ -0,0 +1,21 @@
+1 = Definir variáveis 
+float Nota1, Nota2, média;
+int opc;
+
+2 = Exibir o menu com as opções 
+Opções: 1 - Calcular média, 2 - Determinar status, 3 - Sair
+
+3 = ler a opção selecionada 
+
+4 = fazer o switch menu.
+
+case 1: calcular media ((nota1 + nota2)/2)
+
+case 2: calcular media ((nota1 + nota2)/2) e exibir os status,
+status: >= 7.0 - APROVADO
+status: >= 5.0 && < 7.0 - RECUPERAÇÃO
+status: < 5.0 - REPROVADO
+
+case 3: Sair
+
+Default: erro
--- menu.c
+++ menu.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+int main(){
+
+    float n1, n2, media;
+    int opc;
+
+    printf("Menu de Generenciamento de Estudantes.\n");
+    printf("1 - Calcular média\n");
+    printf("2 - Determinar status\n");
+    printf("3 - Sair\n");
+    scanf("%d", &opc);
+
+    switch (opc){
+    case 1:
+        printf("Digite a nota 1: \n");
+        scanf("%f", &n1);
+        printf("Digite a nota 2: \n");
+        scanf("%f", &n2);
+        media = ((n1+n2)/2);
+        printf("Sua média é: %.2f \n", media);
+
+    break;
+
+    case 2:
+
+        printf("Digite a nota 1: \n");
+        scanf("%f", &n1);
+        printf("Digite a nota 2: \n");
+        scanf("%f", &n2);
+        media = ((n1+n2)/2);
+    if(media >= 7.0){
+        printf("Média: %.2f\n", media);
+        printf("Status: Aprovado!\n");
+    }
+    else if(media >= 5.0 && media < 7.0){
+        printf("Média: %.2f\n", media);
+        printf("Status: Recuperação!\n");
+    }
+    else{
+        printf("Média: %.2f\n", media);
+        printf("Status: Reprovado!\n");
+    }
+
+    break;
+
+    case 3:
+    printf("Saindo...\n");
+
+    break;
+    
+    default:
+    printf("Erro\n");
+        break;
+    }
+
+
+
+
+}
